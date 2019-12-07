fda02ae7fda26a1ae21c60460d6b702aceda4f51
felipeseolin <felipe.seolin@hotmail.com>
2019-12-06 23:06:35 -0300

    Added cache constants

---
+++ src/cache/constants.js
@@ -0,0 +1,12 @@
+const BOARDS = 'BOARDS';
+const ALL_BOARDS = 'ALL_BOARDS';
+const LISTS = 'LISTS';
+const CARDS = 'CARDS';
+
+module.exports.BOARDS = BOARDS;
+module.exports.ALLL_BOARDS = ALL_BOARDS;
+module.exports.BOARDS_ALLL_BOARDS = [BOARDS, ALL_BOARDS];
+module.exports.LISTS = LISTS;
+module.exports.LISTS_BOARDS = [LISTS, BOARDS, ALL_BOARDS];
+module.exports.CARDS = CARDS;
+module.exports.CARDS_LISTS_BOARDS = [CARDS, LISTS, BOARDS];
