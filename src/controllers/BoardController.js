1c2d95919322f8ab3fab5c847877a323398c6fb1
felipeseolin <felipe.seolin@hotmail.com>
2019-12-09 00:32:03 -0300

    Add validation to board

--- src/controllers/BoardController.js
+++ src/controllers/BoardController.js
@@ -12,6 +12,33 @@ const List = mongoose.model('List');
 const Card = mongoose.model('Card');
 const User = mongoose.model('User');
 
+function validate(name, description) {
+  let errors = [];
+
+  if (!name || name.trim().length === 0) {
+    errors = [...errors, 'Um nome deve ser dado ao quadro.'];
+  }
+  if (name && name.length > 100) {
+    errors = [...errors, 'O nome deve ter ao máximo 100 caracteres.'];
+  }
+  if (name && name.length < 4) {
+    errors = [...errors, 'O nome deve ter ao mínimo 4 caracteres.'];
+  }
+  if (description && description.length > 250) {
+    errors = [...errors, 'A descrição deve ter ao máximo 250 caracteres.'];
+  }
+
+  return errors;
+}
+
+function validateAndRedirect(req, res) {
+  const { name, description } = req.body;
+  const error = validate(name, description);
+  if (error.length > 0) {
+    res.status(HTTPCode.BAD_REQUEST).send({ error });
+  }
+}
+
 module.exports = {
   async index(req, res) {
     const boards = await Board.find({ _user: req.userId });
@@ -21,13 +48,19 @@ module.exports = {
     // Find User
     const user = await User.findById(req.userId);
     if (!user) {
-      return res.sendStatus(HTTPCode.UNAUTHORIZED);
+      return res
+        .status(HTTPCode.UNAUTHORIZED)
+        .send({ error: 'Erro ao encontrar usuário' });
     }
+    // Validate Board
+    validateAndRedirect(req, res);
     // Create Board
     const board = await Board.create({ ...req.body, _user: req.userId });
     // Verify if the board is saved
     if (!board) {
-      return res.sendStatus(HTTPCode.BAD_REQUEST);
+      return res
+        .status(HTTPCode.BAD_REQUEST)
+        .send({ error: 'Erro ao criar quadro' });
     }
     // Add board to user
     user.boards.push(board);
@@ -43,6 +76,9 @@ module.exports = {
     return res.status(HTTPCode.OK).json(board);
   },
   async update(req, res) {
+    // Validate Board
+    validateAndRedirect(req, res);
+    req.params._user = undefined;
     // Update Board
     const board = await Board.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
