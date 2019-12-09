f00cb18f34cf874695912f230e2c77d36d59bb84
felipeseolin <felipe.seolin@hotmail.com>
2019-12-09 00:52:25 -0300

    Added list validation

--- src/controllers/ListController.js
+++ src/controllers/ListController.js
@@ -11,12 +11,43 @@ const Board = mongoose.model('Board');
 const Card = mongoose.model('Card');
 const User = mongoose.model('User');
 
+function validate(name, description, board) {
+  let errors = [];
+
+  if (!name || name.trim().length === 0) {
+    errors = [...errors, 'Um nome deve ser dado à lista.'];
+  }
+  if (name && name.length > 100) {
+    errors = [...errors, 'O nome deve ter ao máximo 100 caracteres.'];
+  }
+  if (name && name.length < 2) {
+    errors = [...errors, 'O nome deve ter ao mínimo 2 caracteres.'];
+  }
+  if (description && description.length > 250) {
+    errors = [...errors, 'A descrição deve ter ao máximo 250 caracteres.'];
+  }
+  if (!board) {
+    errors = [...errors, 'A lista deve pertencer a um quadro'];
+  }
+
+  return errors;
+}
+
+function validateAndRedirect(req, res) {
+  const { name, description, _board } = req.body;
+  const error = validate(name, description, _board);
+  if (error.length > 0) {
+    return res.status(HTTPCode.BAD_REQUEST).send({ error });
+  }
+}
+
 module.exports = {
   async index(req, res) {
     const lists = await User.find({ _id: req.userId })
       .select('lists')
       .populate({
         path: 'boards',
+        select: 'lists',
         populate: {
           path: 'lists',
         },
@@ -26,6 +57,13 @@ module.exports = {
   async store(req, res) {
     // Find Board
     const board = await Board.findById(req.body._board);
+    if (!board) {
+      return res
+        .status(HTTPCode.BAD_REQUEST)
+        .send({ error: 'O quadro não existe' });
+    }
+    // Validation
+    validateAndRedirect(req, res);
     // Create List
     const list = await List.create(req.body);
     // Verify if the list is saved
@@ -46,8 +84,11 @@ module.exports = {
     return res.json(list);
   },
   async update(req, res) {
+    const { _board, ...rest } = req.body;
+    // Validation
+    validateAndRedirect(req, res);
     // Create List
-    const list = await List.findByIdAndUpdate(req.params.id, req.body, {
+    const list = await List.findByIdAndUpdate(req.params.id, rest, {
       new: true,
     });
     // Verifica se já salvou
